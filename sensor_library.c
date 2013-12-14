72224408889b8118b8a30ac219cf7aba3244de90
James Dunn <c14james.dunn.ca@usafa.edu>
2013-12-14 14:02:20 -0700

    Sensor Library Completed Code

---
+++ sensor_library.c
@@ -0,0 +1,10 @@
+/*
+ * sensor_library.c
+ *
+ *  Created on: Dec 14, 2013
+ *      Author: C14James.Dunn
+ */
+
+
+
+
---
+++ sensor_library.h
@@ -0,0 +1,14 @@
+/*
+ * sensor_library.h
+ *
+ *  Created on: Dec 14, 2013
+ *      Author: C14James.Dunn
+ */
+
+#ifndef SENSOR_LIBRARY_H_
+#define SENSOR_LIBRARY_H_
+
+
+
+
+#endif /* SENSOR_LIBRARY_H_ */
